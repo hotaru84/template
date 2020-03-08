d23009e972d876d7f919246162a6f8b8b4b441c7
hiroomi ohori <tkgyb713@yahoo.co.jp>
2020-03-08 22:49:50 +0900

    sort

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,24 @@
     <v-data-iterator
       :items="devices"
       :items-per-page.sync="itemsPerPage"
+      :sort-by="sortBy"
+      :sort-desc="sortDesc"
       hide-default-footer
     >
       <template v-slot:header>
       <v-tabs icons-and-text centered grow color="primary">
-        <v-tab>最近の更新<v-icon>date_range</v-icon></v-tab>
-        <v-tab>最もアクティブ<v-icon>touch_app</v-icon></v-tab>
-        <v-tab>アクション数が多い<v-icon>flare</v-icon></v-tab>
-        <v-tab>移動回数が多い<v-icon>directions_walk</v-icon></v-tab>
-        <v-tab>バッテリー残量低<v-icon>battery_alert</v-icon></v-tab>
+        <v-tab @click="changeTab('time')">最近の更新<v-icon>date_range</v-icon></v-tab>
+        <v-tab @click="changeTab('active')">最もアクティブ<v-icon>touch_app</v-icon></v-tab>
+        <v-tab @click="changeTab('action')">アクション数が多い<v-icon>flare</v-icon></v-tab>
+        <v-tab @click="changeTab('step')">移動回数が多い<v-icon>directions_walk</v-icon></v-tab>
+        <v-tab @click="changeTab('battery')">バッテリー残量低<v-icon>battery_alert</v-icon></v-tab>
         <v-tab>MORE<v-icon>tune</v-icon></v-tab>
       </v-tabs>
       </template>
       <template v-slot:default="props">
         <v-row>
           <v-col :md="4" :sm="6" :xs="12" v-for="(item,id) in props.items" :key="id">
-            <device-card :device="item" mainTitle="active"/>
+            <device-card :device="item" :mainTitle="title"/>
           </v-col>
         </v-row>
       </template>
@@ -35,8 +37,18 @@ export default {
   name: 'home',
   data(){
     return {
+      sortBy:'time',
+      sortDesc:true,
+      title:'active',
       itemsPerPage:4,
       items:[1,1,1,1,1,1]
+    }
+  },
+  methods:{
+    changeTab(title) {
+      this.title = title == 'time' ? 'active' : title
+      this.sortDesc = title != 'battery'
+      this.sortBy = title
     }
   },
   computed:{
